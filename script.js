759e47f8c73c5253d9e7675784beb36b9080b692
SagarPrasad160 <sagarprasad916@gmail.com>
2023-06-10 15:11:14 +0530

    Add event listeners to countdownForm submit event

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 const inputContainer = document.getElementById("input-container");
-const inputForm = document.getElementById("countdown-form");
+const countdownForm = document.getElementById("countdown-form");
 const dateEl = document.getElementById("date-picker");
 
 const today = new Date().toISOString().split("T")[0];
 
+let title = "";
+let date = "";
+
 // set date
 dateEl.setAttribute("min", today);
+
+function updateCountdown(e) {
+  e.preventDefault();
+  title = e.srcElement[0].value;
+  date = e.srcElement[1].value;
+  console.log(title, date);
+}
+
+// get countdown title and date
+countdownForm.addEventListener("submit", updateCountdown);
